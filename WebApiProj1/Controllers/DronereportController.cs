211993111f8e86cdc7affc10968d25a3fdfbd33c
michaelmars <markmich@gmail.com>
2020-09-28 22:56:44 +0300

    Add files via upload

---
+++ WebApiProj1/Controllers/DronereportController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApiProj1.Controllers
+{
+
+    [Route("[controller]")]
+    [ApiController]
+    public class DronereportController
+    {
+        [HttpGet("{option}")]
+        public object Get(string option)
+        {
+            try
+            {
+                var options = new JsonSerializerOptions
+                {
+                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+                    WriteIndented = true
+                };
+
+                string filename = "dronereport_option_" + option + ".json";
+                var jsonString = File.ReadAllText("Jsons\\dronereport_option_full.json");
+                return jsonString;
+            }
+            catch
+            {
+            }
+
+            return "error";
+        }
+    }
+}
